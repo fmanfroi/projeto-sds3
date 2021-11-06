84150803aa4894a01a995d8b7104bd8381e51e07
fernando.manfroi <fernando.manfroi@dataprev.gov.br>
2021-11-06 20:56:14 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -7,7 +9,20 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá mundo!</h1>
+        <h1 className="text-primary py-3">DashBoard de vendas</h1>
+        <div className='row px-3'>
+            <div className="col-sm-6">
+              <h5 className='text-center text-secondary'>Taxa de sucesso (%)</h5>
+              <BarChart />              
+            </div>
+            <div className="col-sm-6">
+              <h5 className='text-center text-secondary'>Totas vendas</h5>
+              <DonutChart />              
+            </div>
+        </div>
+        <div className='row px-3'>
+            <h5 className='text-primary'>Todas vendas</h5>
+        </div>
         <DataTable />
       </div>
       <Footer />
