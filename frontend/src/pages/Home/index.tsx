b57a70f6e0544f9e177ea8a240fddabbba56e19a
fernando.manfroi <fernando.manfroi@dataprev.gov.br>
2021-11-07 15:12:52 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-const Home = () => {
-    return (
-        <div>
-           <h2> page home </h2>
-        </div>
-    );
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
-}
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link to="/dashboard" className="btn btn-primary" >
+              Acessar Dashboard
+            </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default Home;
